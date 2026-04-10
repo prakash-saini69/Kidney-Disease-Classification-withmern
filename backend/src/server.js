4efd53904cf0f035f4db93d01224e092393f8665
prakash mali <prakashmali6556@gmail.com>
2026-04-10 17:51:19 +0530

    fix 3

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 
 async function bootstrap() {
   await connectDB(process.env.MONGODB_URI);
-  app.listen(port, () => {
+  app.listen(port, "0.0.0.0", () => {
     console.log(`Backend server running on port ${port}`);
   });
 }
